e47e906e3b542131637026036218cdb631a59733
yagya <yagyagoel87@gmail.com>
2024-02-08 06:00:29 +0530

    added the work component

---
+++ frontend/src/components/Work.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name="work" className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div>
+            <div>
+                <p>work</p>
+                <p>Check out some of my recent work</p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
